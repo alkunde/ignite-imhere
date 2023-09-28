bacfa79e1fb8ec1c1bfc1c26b9e3a30f77dd1c79
André Kunde <andrelbkunde@gmail.com>
2023-09-27 22:07:43 -0300

    using stylesheet

---
+++ App.tsx
@@ -0,0 +1,33 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Nome do evento
+      </Text>
+
+      <Text style={styles.eventDate}>
+        Sexta, 4 de Novembro de 2023.
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#131016',
+    padding: 24,
+  },
+  eventName: {
+    color: '#FDFCFE',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48,
+  },
+  eventDate: {
+    color: '#6B6B6B',
+    fontSize: 16,
+  },
+})
